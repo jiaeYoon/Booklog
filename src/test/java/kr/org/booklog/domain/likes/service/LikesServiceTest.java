18d85f76c6567838bd3b798f7bea4a226dcd6104
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-07-08 17:50:35 +0900

    #17 Fix : 좋아요 취소 테스트 NPE 오류 발생

--- src/test/java/kr/org/booklog/domain/likes/service/LikesServiceTest.java
+++ src/test/java/kr/org/booklog/domain/likes/service/LikesServiceTest.java
@@ -4,8 +4,10 @@ import kr.org.booklog.domain.like.dto.LikesSaveRequestDto;
 import kr.org.booklog.domain.like.entity.Likes;
 import kr.org.booklog.domain.like.repository.LikesRepository;
 import kr.org.booklog.domain.like.service.LikesService;
+import kr.org.booklog.domain.post.dto.PostRequestDto;
 import kr.org.booklog.domain.post.entity.Post;
 import kr.org.booklog.domain.post.repository.PostRepository;
+import kr.org.booklog.domain.post.service.PostService;
 import kr.org.booklog.domain.user.entity.OAuthType;
 import kr.org.booklog.domain.user.entity.User;
 import kr.org.booklog.domain.user.repository.UserRepository;
@@ -27,9 +29,10 @@ class LikesServiceTest {
     @Autowired UserRepository userRepository;
     @Autowired PostRepository postRepository;
     @Autowired LikesService likesService;
+    @Autowired PostService postService;
 
     @Test
-    void saveLikes() {
+    void 좋아요_등록() {
         
         //given : users, posts 미리 세팅
         for (int i = 0; i < 3; i++) {
@@ -71,5 +74,27 @@ class LikesServiceTest {
         assertThat(postRepository.findById(postId).get().getLikesCnt()).isEqualTo(2);   // 두 명이 좋아요한 게시글의 좋아요 수 확인
 
         assertThat(postRepository.findAll().get(0).getLikesCnt()).isEqualTo(0);     // 좋아요하지 않은 게시글의 좋아요 수 확인
+    }
+
+    @Test
+    void 좋아요_취소() {
+
+        //given
+        User user1 = new User("tname", "tpw", "tnickname", "temail@gmail.com", OAuthType.GOOGLE);
+        userRepository.save(user1);
+
+        Long postId = postService.save(new PostRequestDto(user1.getId(), "테스트를 읽고...", "테스트", "zzyoon",
+                LocalDate.of(2023, 5, 14), LocalDate.of(2023, 6, 14), LocalDate.of(2023, 6, 14),
+                4, "감상평 테스트1"));
+
+        Long likesId = likesService.save(postId, new LikesSaveRequestDto(user1.getId(), postId));
+        int repoSize = likesRepository.findAll().size();
+
+        //when
+        likesService.delete(postId, likesId);
+
+        //then
+        assertThat(likesRepository.findAll().size()).isEqualTo(repoSize - 1);
+
     }
 }
