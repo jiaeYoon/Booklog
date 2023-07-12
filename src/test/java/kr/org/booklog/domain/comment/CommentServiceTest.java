587229a5c2c43b958f06736b1b2db7e811039af0
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-07-12 18:28:30 +0900

    #23 Test : 댓글 등록 테스트의 게시글 저장 방식 변경

--- src/test/java/kr/org/booklog/domain/comment/CommentServiceTest.java
+++ src/test/java/kr/org/booklog/domain/comment/CommentServiceTest.java
@@ -5,19 +5,21 @@ import kr.org.booklog.domain.comment.repository.CommentRepository;
 import kr.org.booklog.domain.post.CommentService;
 import kr.org.booklog.domain.post.dto.PostRequestDto;
 import kr.org.booklog.domain.post.repository.PostRepository;
-import kr.org.booklog.domain.post.service.PostService;
+import kr.org.booklog.domain.post.entity.Post;
 import kr.org.booklog.domain.user.entity.OAuthType;
 import kr.org.booklog.domain.user.entity.User;
 import kr.org.booklog.domain.user.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class CommentServiceTest {
 
     @Autowired CommentRepository commentRepository;
@@ -34,10 +36,10 @@ class CommentServiceTest {
         userRepository.save(user);
         Long userId = userRepository.findTop1ByOrderByIdDesc().getId();
 
-        postService.save(new PostRequestDto(userId, "테스트를 읽고...", "테스트", "zzyoon",
+        Post post = new Post(user, "댓글 조회 테스트를 읽고...", "댓글 조회 테스트", "zzyoon",
                 LocalDate.of(2023, 5, 14), LocalDate.of(2023, 6, 14), LocalDate.of(2023, 6, 14),
-                4, "감상평 테스트1"));
-        Long postId = postRepository.findTop1ByOrderByIdDesc().getId();
+                4, "댓글 조회 테스트", 0, 0);
+        Long postId = postRepository.save(post).getId();
 
         // when
         CommentRequestDto requestDto = new CommentRequestDto(userId, postId, "테스트 댓글");
