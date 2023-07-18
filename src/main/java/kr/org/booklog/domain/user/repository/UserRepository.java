41fe1c47ce7adf6144dacf8e280d78e30ba2ab2d
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-07-18 09:13:14 +0900

    #1 Add : 스프링 시큐리티 설정

--- src/main/java/kr/org/booklog/domain/user/repository/UserRepository.java
+++ src/main/java/kr/org/booklog/domain/user/repository/UserRepository.java
@@ -3,6 +3,10 @@ package kr.org.booklog.domain.user.repository;
 import kr.org.booklog.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findTop1ByOrderByIdDesc();
+
+    Optional<User> findByEmail(String email);
 }
