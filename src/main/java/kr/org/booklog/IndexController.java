f06a332b1ec70347d7f228413c1d9f582924a3c6
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-07-18 11:14:57 +0900

    #1 Test : 스프링 시큐리티 적용 테스트

--- src/main/java/kr/org/booklog/IndexController.java
+++ src/main/java/kr/org/booklog/IndexController.java
@@ -22,4 +22,9 @@ public class IndexController {
         }
         return "index";
     }
+
+    @GetMapping("/api/v1/posts/save")
+    public String postsSave() {
+        return "post-save";
+    }
 }
