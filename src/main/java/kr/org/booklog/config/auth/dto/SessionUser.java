ed9a00131329cbd3eb136bc2b275a2fc87964651
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-07-18 09:23:01 +0900

    #1 Feat : 소셜로그인 구현

---
+++ src/main/java/kr/org/booklog/config/auth/dto/SessionUser.java
@@ -0,0 +1,18 @@
+package kr.org.booklog.config.auth.dto;
+
+import kr.org.booklog.domain.user.entity.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+
+    private String name;
+    private String email;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+}
