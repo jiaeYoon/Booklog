eb5706baac5a3a795cbc4d7e845ff2f068b6d236
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-08-21 16:52:50 +0900

    Test : 좋아요 등록 및 취소 테스트

--- src/main/java/kr/org/booklog/domain/like/repository/LikesRepository.java
+++ src/main/java/kr/org/booklog/domain/like/repository/LikesRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface LikesRepository extends JpaRepository<Likes, Long> {
+
+    @Query("select l from Likes l where post_id = :postId")
+    List<Likes> findByPostId(@Param("postId") Long postId);
+
     Optional<Likes> findByUserIdAndPostId(Long userId, Long postId);
 
     @Query("select l from Likes l where user_id = :userId and post_id = :postId")
--- src/test/java/kr/org/booklog/domain/likes/service/LikesServiceTest.java
+++ src/test/java/kr/org/booklog/domain/likes/service/LikesServiceTest.java
@@ -1,7 +1,5 @@
 package kr.org.booklog.domain.likes.service;
 
-import kr.org.booklog.domain.like.dto.LikesSaveRequestDto;
-import kr.org.booklog.domain.like.entity.Likes;
 import kr.org.booklog.domain.like.repository.LikesRepository;
 import kr.org.booklog.domain.like.service.LikesService;
 import kr.org.booklog.domain.post.entity.Post;
@@ -17,6 +15,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,57 +35,45 @@ class LikesServiceTest {
     void 좋아요_등록() {
 
         //given : users, posts 미리 세팅
-        Long userId = newUsers(2);
-        Long postId = newPosts(userId, 3);
+        List<Long> userIds = newUsers(2);
+        Long userId1 = userIds.get(0);
+        Long userId2 = userIds.get(1);
+        Long postId = newPosts(userId1, 3);
 
+        em.flush(); // post 엔티티의 default value 적용을 위한 플러시 및 영속성 컨텍스트 초기화
         em.clear();
 
-        // when
-        // 유저 2명이 한 포스트에 좋아요 등록
-        LikesSaveRequestDto requestDto1 = new LikesSaveRequestDto();
-        requestDto1.setUserId(userId);
-
-        LikesSaveRequestDto requestDto2 = new LikesSaveRequestDto();
-        requestDto2.setUserId(userRepository.findAll().get(1).getId());
-
-        Long likesId = likesService.save(postId, requestDto1);
-        Long likesId2 = likesService.save(postId, requestDto2);
-
-        em.flush();
-        em.clear();
+        // when : 유저 2명이 한 포스트에 좋아요 등록
+        Long likesId = likesService.save(postId, userId1);
+        Long likesId2 = likesService.save(postId, userId2);
 
         //then
-        Likes like = likesRepository.findById(likesId).get();
-
-        assertThat(like.getUser().getId()).isEqualTo(userId);
-        assertThat(like.getIsLike()).isEqualTo(Boolean.TRUE);
-        assertThat(postRepository.findById(postId).get().getLikesCnt()).isEqualTo(2);   // 두 명이 좋아요한 게시글의 좋아요 수 확인
+        assertThat(likesRepository.findByPostId(postId).size()).isEqualTo(2);
+        assertThat(postRepository.findById(postId).get().getLikesCnt()).isEqualTo(2);
     }
 
     @Test
     void 좋아요_취소() {
 
         //given
-        Long userId = newUsers(1);
+        Long userId = newUsers(1).get(0);
         Long postId = newPosts(userId,1);
+
+        em.flush(); // post 엔티티의 default value 적용을 위한 플러시 및 영속성 컨텍스트 초기화
         em.clear();
 
-        Long likesId = likesService.save(postId, new LikesSaveRequestDto(userId));
-        int repoSize = likesRepository.findAll().size();
+        Long likesId = likesService.save(postId, userId);
 
         //when
         likesService.delete(postId, likesId);
 
-        em.flush();
-        em.clear();
-
         //then
-        assertThat(likesRepository.findAll().size()).isEqualTo(repoSize - 1);
+        assertThat(likesRepository.findByPostId(postId).size()).isEqualTo(0);
         assertThat(postRepository.findById(postId).get().getLikesCnt()).isEqualTo(0);
     }
 
-    private Long newUsers(int num) {
-        Long id = null;
+    private List<Long> newUsers(int num) {
+        List<Long> ids = new ArrayList<>();
         for (int i = 0; i < num; i++) {
             User user = User.builder()
                     .name("이름" + i)
@@ -92,9 +81,9 @@ class LikesServiceTest {
                     .email("email" + i + "@gmail.com")
                     .role(Role.USER)
                     .build();
-            id = userRepository.save(user).getId();
+            ids.add(userRepository.save(user).getId());
         }
-        return id;
+        return ids;
     }
 
     private Long newPosts(Long userId, int num) {
@@ -110,7 +99,7 @@ class LikesServiceTest {
                     .bookWriter("zzyoon")
                     .readStart(LocalDate.of(2023, 5, 14))
                     .readEnd(LocalDate.of(2023, 6, 14))
-                    .postAt(LocalDate.of(2023, 6, 14))
+                    .postAt(LocalDateTime.now())
                     .rating(4)
                     .content("후기" + i)
                     .build();
