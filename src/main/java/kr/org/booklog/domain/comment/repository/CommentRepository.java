606df7e40c234c80cd590a1f5e0c7d0b8fe9a412
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-08-22 21:34:05 +0900

    #32 Fix : 댓글 조회 시, 발생하는 N + 1 문제 해결
    댓글 작성자의 닉네임을 조회하는 과정에서 쿼리가 N + 1개 발생하는 문제 발생. 페치 조인으로 해결함

--- src/main/java/kr/org/booklog/domain/comment/repository/CommentRepository.java
+++ src/main/java/kr/org/booklog/domain/comment/repository/CommentRepository.java
@@ -2,9 +2,13 @@ package kr.org.booklog.domain.comment.repository;
 
 import kr.org.booklog.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findByPostId(Long id);
+
+    @Query("select c from Comment c join fetch c.user where post_id = :postId")
+    List<Comment> findByPostId(@Param("postId") Long postId);
 }
