01fd7b65a8fcae280afb007ff92161299bd00264
jiaeYoon <yoon529@sookmyung.ac.kr>
2023-11-14 21:51:21 +0900

    #35 Test : 독서 모임 가입 테스트

--- src/test/java/kr/org/booklog/domain/club/service/BookClubServiceTest.java
+++ src/test/java/kr/org/booklog/domain/club/service/BookClubServiceTest.java
@@ -4,6 +4,8 @@ import kr.org.booklog.config.auth.dto.SessionUser;
 import kr.org.booklog.domain.club.dto.ClubCreateRequestDto;
 import kr.org.booklog.domain.club.entity.Club;
 import kr.org.booklog.domain.club.repository.ClubRepository;
+import kr.org.booklog.domain.memberRegister.MemberRegister;
+import kr.org.booklog.domain.memberRegister.MemberRegisterRepository;
 import kr.org.booklog.domain.user.entity.Role;
 import kr.org.booklog.domain.user.entity.User;
 import kr.org.booklog.domain.user.repository.UserRepository;
@@ -21,8 +23,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Transactional
 class BookClubServiceTest {
 
-    @Autowired EntityManager em;
     @Autowired UserRepository userRepository;
+    @Autowired MemberRegisterRepository memberRegisterRepository;
 
     @Autowired ClubRepository clubRepository;
     @Autowired ClubService clubService;
@@ -32,7 +34,7 @@ class BookClubServiceTest {
     void save() {
 
         //given
-        SessionUser sessionUser = newSessionUser();
+        SessionUser sessionUser = new SessionUser(newUser("A"));
         ClubCreateRequestDto requestDto = ClubCreateRequestDto.builder()
                 .clubName("추리 소설 클럽")
                 .capacity(4)
@@ -49,15 +51,47 @@ class BookClubServiceTest {
         assertThat(bookClub.getLeader().getNickname()).isEqualTo(sessionUser.getNickname());
     }
 
-    private SessionUser newSessionUser() {
+    @Test
+    @DisplayName("독서 모임 참가")
+    void join() {
+
+        //given
+        User leader = newUser("A");
+        User member = newUser("B");
+
+        Long clubId = newClub(leader).getId();
+
+        //when
+        Long bookClubMemberId = clubService.join(new SessionUser(member), clubId);
+
+        //then
+        MemberRegister memberRegister = memberRegisterRepository.findById(bookClubMemberId).get();
+        Club club = clubRepository.findById(clubId).get();
+        User user = userRepository.findById(member.getId()).get();
+
+        assertThat(user.getMemberRegisters().size()).isEqualTo(1);
+        assertThat(memberRegister.getClub().getId()).isEqualTo(clubId);
+        assertThat(memberRegister.getUser().getName()).isEqualTo("B");
+    }
+
+    private Club newClub(User leader) {
+        Club club = Club.builder()
+                .leader(leader)
+                .clubName("추리 소설 클럽")
+                .capacity(4)
+                .introduction("소개글")
+                .build();
+        return clubRepository.save(club);
+    }
+
+    private User newUser(String name) {
         User user = User.builder()
-                .name("유저A")
-                .nickname("닉네임")
-                .email("email@gmail.com")
+                .name(name)
+                .nickname("닉네임" + name)
+                .email(name + "@gmail.com")
                 .role(Role.USER)
                 .build();
         userRepository.save(user);
-
-        return new SessionUser(user);
+        return user;
     }
 }
